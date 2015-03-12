50efef3683b575543a90299cdf382c792b439cf8
bvnbase <bvnbase@gmail.com>
2015-03-12 17:42:29 +0700

    + Fix bug cannot find Shipping tab

--- app/overrides/add_menu_tabs.rb
+++ app/overrides/add_menu_tabs.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => 'spree/admin/shared/_tabs',
   :name => 'add_menu_tabs',
   :insert_after => "erb[silent]:contains('Spree::Order')",
   :text => "
-    #<% if can? :admin, Spree::Order %>
-    #  <%= tab *Spree::BackendConfiguration::SHIPPING_TABS, icon: 'shopping-cart' %>
-    #<% end %>
+    <!--<% if can? :admin, Spree::Order %>
+      <%= tab *Spree::BackendConfiguration::SHIPPING_TABS, icon: 'shopping-cart' %>
+    <% end %>-->
   ")
