42f48f93e074a7389c2a00c53153bc8dd19ed62f
bvnbase <bvnbase@gmail.com>
2015-03-12 17:45:39 +0700

    + Fix bug

--- app/overrides/add_menu_tabs.rb
+++ app/overrides/add_menu_tabs.rb
@@ -2,7 +2,4 @@ Deface::Override.new(:virtual_path => 'spree/admin/shared/_tabs',
   :name => 'add_menu_tabs',
   :insert_after => "erb[silent]:contains('Spree::Order')",
   :text => "
-    <!--<% if can? :admin, Spree::Order %>
-      <%= tab *Spree::BackendConfiguration::SHIPPING_TABS, icon: 'shopping-cart' %>
-    <% end %>-->
   ")
