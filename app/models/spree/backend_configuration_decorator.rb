267de0dc6c9fd47440b2f65ebe5e414234c8a56a
bvnbase <bvnbase@gmail.com>
2015-03-12 15:51:27 +0700

    + Fix error when  modify CMS

--- app/models/spree/backend_configuration_decorator.rb
+++ app/models/spree/backend_configuration_decorator.rb
@@ -1,6 +1,6 @@
 module Spree
 
-	 BackendConfiguration.const_set(:SHIPPING_TABS, [:shipping, :payments, :creditcard_payments,:shipments, :credit_cards, :return_authorizations,:customer_returns, :adjustments, :customer_details])
+	 #BackendConfiguration.const_set(:SHIPPING_TABS, [:shipping, :payments, :creditcard_payments,:shipments, :credit_cards, :return_authorizations,:customer_returns, :adjustments, :customer_details])
 
 	 BackendConfiguration.class_eval do 
 	 		
