66604745bf23c5e99798f4d0f1a19aa2a0ab653c
bvnbase <bvnbase@gmail.com>
2015-03-15 18:20:09 +0700

    + Add menu tab

--- app/overrides/add_tabs.rb
+++ app/overrides/add_tabs.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => 'spree/layouts/admin',
   :name => 'add_tabs',
-  :insert_after => "erb[loud]:contains('spree/admin/shared/header')",
+  :insert_before => "erb[loud]:contains('spree/admin/shared/header')",
   :text => "
     <ul class='nav nav-sidebar'>
     <li>Bvnkame</li>
