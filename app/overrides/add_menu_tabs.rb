ccdcd9d35f0e51f992cee3131a227b7a89ccadfc
bvnbase <bvnbase@gmail.com>
2015-03-15 19:30:58 +0700

    + Test add new tab to admin CMS

--- app/overrides/add_menu_tabs.rb
+++ app/overrides/add_menu_tabs.rb
@@ -1,5 +1,8 @@
 Deface::Override.new(:virtual_path => 'spree/admin/shared/_tabs',
   :name => 'add_menu_tabs',
-  :insert_after => "erb[silent]:contains('Spree::Order')",
+  :insert_before => "erb[silent]:contains('Spree::Admin::ReportsController')",
   :text => "
+    <% if can? :admin, Spree::Order %>
+      <%= tab *Spree::BackendConfiguration::ORDER_TABS, icon: 'shopping-cart' %>
+    <% end %>
   ")
