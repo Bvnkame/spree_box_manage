39596a5ea89a7055712ad86ad6b352f6f3a127e1
bvnbase <bvnbase@gmail.com>
2015-03-16 11:49:41 +0700

    + Commit for test

--- app/models/spree/backend_configuration_decorator.rb
+++ app/models/spree/backend_configuration_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
 
-	 #BackendConfiguration.const_set(:SHIPPING_TABS, [:shipping, :payments, :creditcard_payments,:shipments, :credit_cards, :return_authorizations,:customer_returns, :adjustments, :customer_details])
-
+	 BackendConfiguration.const_set(:SHIPPING_TABS, [:shipping, :payments, :creditcard_payments,:shipments, :credit_cards, :return_authorizations,:customer_returns, :adjustments, :customer_details])
+	 
 	 BackendConfiguration.class_eval do 
 	 		def abc
         puts 999999
