de2bf35695a8f4286c48e424e851edfbb8c2737d
bvnbase <bvnbase@gmail.com>
2015-03-16 10:43:17 +0700

    + Add reference from Spree_Box_Details to Spree_Products & Spree_Boxes

--- db/migrate/20150316025408_create_spree_box_details.rb
+++ db/migrate/20150316025408_create_spree_box_details.rb
@@ -1,7 +1,8 @@
 class CreateSpreeBoxDetails < ActiveRecord::Migration
   def change
     create_table :spree_box_details do |t|
-
+      t.references :spree_products
+      t.references :spree_boxes
       t.timestamps
     end
   end
