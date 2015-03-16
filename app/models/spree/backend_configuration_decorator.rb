972fa9db8be58c5d9db8c0eb20ff92f8cc119b09
bvnbase <bvnbase@gmail.com>
2015-03-16 11:42:04 +0700

    some debug code

--- app/models/spree/backend_configuration_decorator.rb
+++ app/models/spree/backend_configuration_decorator.rb
@@ -3,7 +3,9 @@ module Spree
 	 #BackendConfiguration.const_set(:SHIPPING_TABS, [:shipping, :payments, :creditcard_payments,:shipments, :credit_cards, :return_authorizations,:customer_returns, :adjustments, :customer_details])
 
 	 BackendConfiguration.class_eval do 
-	 		
+	 		def abc
+        puts 999999
+      end
 	 end
 
 end
--- app/models/spree_box_detail.rb
+++ app/models/spree_box_detail.rb
@@ -1,4 +1,4 @@
 class SpreeBoxDetail < ActiveRecord::Base
   belongs_to :spree_products
-  belongs_to :spree_boxes
+  belongs_to :spree_boxes, :class_name => "SpreeBox"
 end
