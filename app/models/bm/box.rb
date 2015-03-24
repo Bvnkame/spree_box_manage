d2b87e7893cd4ea3a2607e4f9263a3b6820e9f56
bvnbase <bvnbase@gmail.com>
2015-03-24 17:22:51 +0700

    + Update Bm::Box model properties :users to belong_to

--- app/models/bm/box.rb
+++ app/models/bm/box.rb
@@ -2,7 +2,7 @@ module Bm
   class Box < ActiveRecord::Base
     self.table_name = "bm_boxes"
     has_many :products , through: :boxdetails, :class_name => "Spree::Product"
-    has_one :users, :class_name => "Spree::User", foreign_key: 'spree_user_id'
+    belongs_to :users, :class_name => "Spree::User", foreign_key: 'spree_user_id'
     belongs_to :difficulties, :class_name => "Bm::Dificulty", foreign_key: 'difficulty_id'
     belongs_to :experts, :class_name => "Bm::Expert", foreign_key: 'bm_expert_id'
     has_many :boxdetails, :class_name => "Bm::BoxDetail", foreign_key: 'bm_box_id'
