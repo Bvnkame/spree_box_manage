d22df8aeebbe8d6c113c4bee8c99f5b1b01e2c56
bvnbase <bvnbase@gmail.com>
2015-03-24 15:09:06 +0700

    Fix migrate column

--- db/migrate/20150324024801_box_update_column_difficulty_to_connect_with_dificulies.rb
+++ db/migrate/20150324024801_box_update_column_difficulty_to_connect_with_dificulies.rb
@@ -1,6 +1,6 @@
 class BoxUpdateColumnDifficultyToConnectWithDificulies < ActiveRecord::Migration
   def change
-    rename_column :bm_boxes, :difficulty, :difficulty_id
+    rename_column :bm_boxes, :dificulty, :difficulty_id
     change_column :bm_boxes, :difficulty_id, 'integer USING CAST(difficulty_id AS integer)'
   end
 end
