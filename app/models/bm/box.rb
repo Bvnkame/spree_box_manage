302fefacc50728ec8aa36f7a6d5fe8db705377eb
bvnbase <bvnbase@gmail.com>
2015-04-21 16:48:45 +0700

    fix bug edit expert

--- app/models/bm/box.rb
+++ app/models/bm/box.rb
@@ -3,7 +3,7 @@ module Bm
     self.table_name = "bm_boxes"
     has_many :products , through: :boxdetails, :class_name => "Spree::Product"
     belongs_to :users, :class_name => "Spree::User", foreign_key: 'spree_user_id'
-    belongs_to :experts, :class_name => "Bm::Expert", foreign_key: 'bm_expert_id'
+    belongs_to :expert, :class_name => "Bm::Expert", foreign_key: 'bm_expert_id'
     has_many :boxdetails, :class_name => "Bm::BoxDetail", foreign_key: 'bm_box_id'
   
     has_many :covers, as: :viewable, dependent: :destroy, class_name: "Bm::Imagecover"
